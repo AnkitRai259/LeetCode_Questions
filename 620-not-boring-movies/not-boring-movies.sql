a72d80ba8daf216cab069826cecc622246244a74
Ankit Rai <ankittrai0410@gmail.com>
2025-09-27 22:47:37 +0530

    Time: 221 ms (83.50%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-Select id, movie,description,rating
-from Cinema
-having id%2=1 and description<>'boring'
-order by rating desc;
+SELECT id , movie, description, rating
+FROM Cinema
+WHERE id%2<>0 and description<>"boring"
+ORDER BY rating desc
