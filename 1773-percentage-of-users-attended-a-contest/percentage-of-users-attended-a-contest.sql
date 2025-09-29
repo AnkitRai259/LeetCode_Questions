a624925892e1ce1ed71b3ecbf0749b3a63bcdfe2
Ankit Rai <ankittrai0410@gmail.com>
2025-09-29 23:23:19 +0530

    Time: 1029 ms (38.63%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,11 +1,7 @@
+SELECT contest_id, ROUND(Count(r.user_id)*100/(SELECT COUNT(u.user_id) from Users u),2) as percentage
+FROM Register r 
+group by contest_id
+order by percentage desc, contest_id
 
-With cte as (
-     Select count(user_id) 
-     from Users u)
-
-Select r.contest_id, Round((count(r.user_id)/(select * from cte) *100),2) as percentage
-from Register r join Users u on r.user_id=u.user_id
-Group by r.contest_id
-order by  percentage desc,r.contest_id asc;
 
 
