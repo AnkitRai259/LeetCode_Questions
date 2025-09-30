742c90efcda87eadc2f7a6132777bf3e243da72e
Ankit Rai <ankittrai0410@gmail.com>
2025-09-30 23:18:00 +0530

    Time: 368 ms (41.00%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-with cte as (Select query_name, avg(rating/position) as quality
+With q1 as
+(SELECT query_name, ROUND(AVG(rating/position),2) as quality
 from Queries
-group by query_name),
- cte2 as (Select query_name , SUM(CASE WHEN rating<3 THEN 1 ELSE 0 END)*100.0/ COUNT(*) as poor_query
-from Queries
-group by query_name) 
-select cte.query_name, round(quality,2) as quality , round(poor_query,2) as poor_query_percentage
-from cte join cte2 on cte.query_name = cte2.query_name
-group by query_name;
+GROUP BY query_name)
+, q2 as ( SELECT query_name, ROUND(AVG(CASE WHEN rating <3 then 1 else 0 end)*100,2) as poor_query_percentage
+FROM queries
+group by query_name)
+SELECT q1.query_name, quality, poor_query_percentage
+FROM q1 join q2 on q1.query_name=q2.query_name
