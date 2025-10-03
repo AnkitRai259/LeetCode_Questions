56cc5ff99b134b4f749b45db69035758171236e4
Ankit Rai <ankittrai0410@gmail.com>
2025-10-03 23:20:26 +0530

    Time: 1123 ms (12.94%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
 With stat as (
-SELECT customer_id, CASE WHEN order_date=customer_pref_delivery_date then "immediate" else "scheduled" end as status, ROW_NUMBER() OVER (PARTITION BY customer_id order by order_date) as rnk
+SELECT customer_id,
+       CASE WHEN order_date=customer_pref_delivery_date then "immediate" else "scheduled" end as status, 
+       ROW_NUMBER() OVER (PARTITION BY customer_id order by order_date) as rnk
 FROM Delivery
 )
- Select ROUND(SUM(CASE WHEN status = "immediate" then 1 else 0 end )*100/count(*),2) as immediate_percentage
+Select ROUND(SUM(CASE WHEN status = "immediate" then 1 else 0 end )*100/count(*),2) as immediate_percentage
 FROM stat
 WHERE rnk =1; 
